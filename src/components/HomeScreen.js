54ff041fe34fb8b30413d862f76d5f7d4aae216c
Anuja Nimesh <64465747+aanujanimesh@users.noreply.github.com>
2022-01-19 09:47:38 +0530

    initial

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const HomeScreen = () => {
+  
+  let location = useLocation();
+
   return (
     <div>
       <br />
-      <h1>Welcome to HomeScreen</h1>
+      <h1>Welcome {location.state} </h1>
+      
     </div>
   );
 };
--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,17 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const LoginScreen = () => {
+  let navigate = useNavigate();
+  const [username, setUsername] = useState("");
+
   return (
     <div className="text-center m-5-auto">
       <br />
       <h2 className="h">Sign in to us</h2>
       <form>
         <p>
-          <label>Username or email address</label>
+          <label>Username</label>
           <br />
-          <input type="text" name="first_name" required />
+          <input
+            type="text"
+            name="first_name"
+            required="required"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </p>
         <p>
           <label>Password</label>
@@ -20,11 +29,15 @@ const LoginScreen = () => {
           <input type="password" name="password" required />
         </p>
         <p>
-          <Link to="/homepage">
-            <button id="sub_btn" type="submit">
-              Login
-            </button>
-          </Link>
+          
+          <button
+            id="sub_btn"
+            type="submit"
+            onClick={() => navigate("/homepage", { state: username })}
+          >
+            Login
+          </button>
+          
         </p>
       </form>
     </div>
